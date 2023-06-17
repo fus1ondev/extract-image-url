13199dc852f8b757b023797fcd994354a36c9662
fus1ondev <luke_s1220@yahoo.co.jp>
2023-06-17 20:44:35 +0900

    feat: 🎸 Add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+/**
+ * Extracts the first image url from a markdown string
+ * @param {string} markdown The markdown string to extract image url
+ */
 export const extractImgUrl = (markdown: string) => {
   const match = markdown.match(/.*<img .*src=(['"])(.*?)\1.*>.*|\!\[[^\]]*\]\(\s*([^\s\)]+)\s*(?:[\s]+"[^"]*")?\)/);
   return match ? match[2] ?? match[3] : undefined;
